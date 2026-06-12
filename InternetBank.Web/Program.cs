4226a0a04c7e1372fb40250957912891941b697b
Genadly <dimaredzko21@gmail.com>
2026-06-12 18:10:47 +0300

    refactor(web): replace static HTML with Razor Pages, move CSS colors to variables

--- InternetBank.Web/Program.cs
+++ InternetBank.Web/Program.cs
@@ -5,10 +5,12 @@ namespace InternetBank.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
-
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapRazorPages();
             app.Run();
         }
     }
